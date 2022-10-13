612a43d904fbfd10c5aeb2bb9acc98767f643e36
hieuneuer111 <84337145+hieuneuer111@users.noreply.github.com>
2022-10-13 21:37:38 +0700

    end get  trending movies

--- Server/router/rtMovies.js
+++ Server/router/rtMovies.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const routes = express.Router();
 const moviesController = require("../controllers/moviesController");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-routes.get("/", urlencodedParser, moviesController.getAllMovies);
-routes.get("/:page", urlencodedParser, moviesController.getPageMovies);
+// const urlencodedParser = bodyParser.urlencoded({ extended: false });
+routes.get("/trending", moviesController.getPageMoviesTrending);
+routes.get("/trending/:page", moviesController.getPageMoviesTrending);
 
 module.exports = routes;
